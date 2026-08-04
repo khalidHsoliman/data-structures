4f202edb3772dd0cdb883db8dbf19c3dd553424e
khalidHsoliman <khalidhsoliman@gmail.com>
2026-08-04 06:00:23 +0300

    change domain name

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://data-structures.pages.dev',
+	site: 'https://cpp-data-structures.pages.dev',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
